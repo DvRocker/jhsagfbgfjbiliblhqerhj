85919e520b380cd39ad67f1c1b9ac64cae64681b
DvRocker <42909244+DvRocker@users.noreply.github.com>
2018-09-09 22:43:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,36 +8,6 @@ client.on('ready', () => {
   console.log('---------------')
 });
 
-const devs = ['358529506052669440' , '' , '' , ''];
-const adminprefix = *";
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!devs.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/حب بلا حدود");
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  }
-  });
-
-
-
-
-
-
-
 
 client.on('message', message => {
         if (message.content.startsWith(prefix + "uptime")) {
